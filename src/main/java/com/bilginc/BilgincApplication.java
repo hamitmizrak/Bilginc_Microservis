a88bd803a9ead538d2ff53fa794cddf1705e4854
hamitmizrak <hamitmizrak@gmail.com>
2022-12-12 10:05:47 +0300

    Initial commit

---
+++ src/main/java/com/bilginc/BilgincApplication.java
@@ -0,0 +1,13 @@
+package com.bilginc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BilgincApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BilgincApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/bilginc/BilgincApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bilginc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BilgincApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
